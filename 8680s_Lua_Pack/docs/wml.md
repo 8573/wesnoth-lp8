40698b8406c2309d0d233ec5cacf88fb04aff9df
8573 <8573dd@gmail.com>
2013-03-24 19:35:33 -0700

    Add wml/{is_tag,tags_equal,subtags,children}; extend wml/match_tag.
    Make match_tag able to filter on the tag’s contents being a superset of
    another tag.
    
    Explicitly document match_tag.

--- 8680s_Lua_Pack/docs/wml.md
+++ 8680s_Lua_Pack/docs/wml.md
@@ -2,16 +2,7 @@
 For more easily manipulating WML objects (`cfg`s) in Lua.
 ===============================================================================
 
-A `filter` can be a string, a function, a Boolean, a list of filters, or `nil`.
-
-* A string matches if it equals the subtag’s tag name.
-* A function matches if `filter(subtag)` is truthy.
-* A list matches if any of its contained filters match.
-* A Boolean matches if it is true.
-* `nil` always matches (like an empty filter in WML).
-
-
-An `index` can be omitted, in which case it defaults to 1.
+An `index` argument can be omitted, in which case it defaults to 1.
 
 
 **NOTE**: When I say “tag” or “subtag”, I mean the whole thing, e.g.:
@@ -21,6 +12,36 @@ An `index` can be omitted, in which case it defaults to 1.
 Not just the contents, like what `helper.get_child` returns, e.g.:
 
 	{ side=1, amount=100 }
+
+
+`lp8.is_tag(x)`
+-------------------------------------------------------------------------------
+Returns whether `x` is (or at least looks like) a WML tag object.
+
+
+`lp8.match_tag(tag, filter)`
+-------------------------------------------------------------------------------
+Returns whether `tag` matches `filter`.
+
+`filter` can be a string, a function, a Boolean, a WML tag object, a list of
+filters, or nil.
+
+* A string filter matches if it equals the tag’s name.
+* A function filter matches if `filter(tag)` is truthy.
+* A tag object filter matches if `tag` contains its contents; that is, for each
+  key in `filter`, `tag` has a key with the same name and value, and, for each
+  subtag in `filter`, `tag` has a subtag with the same name for which
+  `match_tag(tag_subtag, filter_subtag)` returns true.
+* A list filter matches if any of its contained filters match.
+* A Boolean filter matches if it is true.
+* A nil filter always matches (like an empty filter in WML).
+
+This is the function used to match tags to filters by the other functions.
+
+
+`lp8.tags_equal(tag1, tag2)`
+-------------------------------------------------------------------------------
+Returns whether `tag1` and `tag2` are structurally equal.
 
 
 `lp8.get_subtag(cfg, filter, index)`
@@ -42,6 +63,16 @@ Returns a list containing each matching subtag of `cfg`.
 `lp8.get_children(cfg, filter)`
 -------------------------------------------------------------------------------
 Returns a list containing the contents of each matching subtag of `cfg`.
+
+
+`lp8.subtags(cfg, filter)`
+-------------------------------------------------------------------------------
+Returns an iterator over all matching subtags of `cfg`.
+
+
+`lp8.chilren(cfg, filter)`
+-------------------------------------------------------------------------------
+Returns an iterator over the contents of all matching subtags of `cfg`.
 
 
 `lp8.remove_subtag(cfg, filter, index)`
@@ -71,5 +102,4 @@ Returns a list containing the contents of each removed subtag.
 `lp8.erase_subtags(cfg, filter)`
 -------------------------------------------------------------------------------
 Like `remove_subtags`, but doesn’t save the erased subtags.
-
 
--- 8680s_Lua_Pack/wml.lua
+++ 8680s_Lua_Pack/wml.lua
@@ -1,85 +1,79 @@
 
 -- By 8680.
 
-local function match(t, f)
-	local ty = type(f)
-	if ty == 'string' then
-		return t[1] == f
-	elseif ty == 'function' then
-		return f(t)
-	elseif ty == 'table' then
-		for i = 1, #f do
-			if match(t, f[i]) then
-				return true
-			end
-		end
-	elseif ty == 'boolean' then
-		return f
-	else
-		return f == nil or error(ty .. "s as filters not supported", 2)
-	end
-end
-lp8.match_tag = match
+local match
 
-local function getSubtag(cfg, f, n)
+local function getSubtag(cfg, f, n, i)
 	n = n or 1
-	for i = 1, #cfg do
+	for i = i or 1, #cfg do
 		if match(cfg[i], f) then
 			n = n-1
 			if n <= 0 then
-				return cfg[i]
+				return cfg[i], i
 			end
 		end
 	end
 end
 lp8.get_subtag = getSubtag
 
+function lp8.subtags(cfg, f, i)
+	return function(s)
+		local t, i = getSubtag(cfg, f, 1, s.i)
+		s.i = i
+		return t, i
+	end, {i = i or 1}
+end
+
+function lp8.children(cfg, f, i)
+	return function(s)
+		local c, i = getSubtag(cfg, f, 1, s.i)
+		s.i = i
+		return c[2], i
+	end, {i = i or 1}
+end
+
 function lp8.get_subtags(cfg, f)
 	local r = {}
-	for i = 1, #cfg do
-		if match(cfg[i], f) then
-			r[#r+1] = cfg[i]
-		end
+	for t in lp8.subtags(cfg, f) do
+		r[#r+1] = t
 	end
 	return r
 end
 
-function lp8.get_child(cfg, f, n)
-	return getSubtag(cfg, f, n)[2]
+function lp8.get_child(cfg, f, n, i)
+	cfg, i = getSubtag(cfg, f, n, i)
+	return cfg[2], i
 end
 
 function lp8.get_children(cfg, f)
 	local r = {}
-	for i = 1, #cfg do
-		if match(cfg[i], f) then
-			r[#r+1] = cfg[i][2]
-		end
+	for c in lp8.children(cfg, f) do
+		r[#r+1] = c
 	end
 	return r
 end
 
 local tr = table.remove
 
-local function removeSubtag(cfg, f, n)
+local function removeSubtag(cfg, f, n, i)
 	n = n or 1
-	for i = 1, #cfg do
+	for i = i or 1, #cfg do
 		if match(cfg[i], f) then
 			n = n-1
 			if n <= 0 then
 				local t = cfg[i]
 				tr(cfg, i)
-				return t
+				return t, i
 			end
 		end
 	end
 end
 lp8.remove_subtag = removeSubtag
 
-function lp8.remove_child(cfg, f, n)
-	return removeSubtag(cfg, f, n)[2]
+function lp8.remove_child(cfg, f, n, i)
+	cfg, i = removeSubtag(cfg, f, n, i)
+	return cfg[2], i
 end
-
-lp8.require "utils"
 
 function lp8.remove_subtags(cfg, f)
 	local r = {}
@@ -111,3 +105,57 @@ function lp8.erase_subtags(cfg, f)
 	end
 end
 
+local function isTag(x)
+	if type(x) ~= 'userdata' and type(x) ~= 'table' then
+		return false
+	end
+	local s, c = pcall(function() return type(x[1]) == 'string' and x[2] end)
+	return s and c and (type(c) == 'table' or type(c) == 'userdata')
+end
+lp8.is_tag = isTag
+
+function match(t, f)
+	local ty = type(f)
+	if ty == 'string' then
+		return t[1] == f
+	elseif ty == 'function' then
+		return f(t)
+	elseif isTag(f) then
+		if t[1] ~= f[1] then
+			return false
+		end
+		t, f = t[2], f[2]
+		for k, v in pairs(f) do
+			if type(k) == 'string' and t[k] ~= v then
+				return false
+			end
+		end
+		local n, i = {}, {}
+		for k, v in ipairs(f) do
+			if isTag(v) then
+				k = v[1]; v, i[k] = getSubtag(t, v, n[k], i[k])
+				if v then
+					tn[k] = (tn[k] or 1) + 1
+				else
+					return false
+				end
+			end
+		end
+		return true
+	elseif ty == 'table' then
+		for i = 1, #f do
+			if match(t, f[i]) then
+				return true
+			end
+		end
+		return false
+	elseif ty == 'boolean' then
+		return f
+	end
+	return f == nil or error(ty .. "s as filters not supported", 2)
+end
+lp8.match_tag = match
+
+function lp8.tags_equal(x, y)
+	return match(x, y) and match(y, x)
+end
