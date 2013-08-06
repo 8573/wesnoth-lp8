b970fad37ee65aa6a6fc17a6baf8e418ba2af71f
8573 <8573dd@gmail.com>
2013-08-06 23:09:08 +0000

    Add `load` library.

--- 8680s_Lua_Pack/_load.lua
+++ 8680s_Lua_Pack/_load.lua
@@ -6,10 +6,16 @@ if not namespace_of_8680s_Lua_Pack then
 	lp8 = lp8 == nil and namespace_of_8680s_Lua_Pack or error(
 		"8680s_Lua_Pack initialization aborted: global variable lp8 already taken!", 0)
 	lp8.helper = wesnoth.require "lua/helper.lua"
-	function lp8.require(script)
+	lp8.lp8dir = dir_of_8680s_Lua_Pack
+	function lp8.require(lib)
 		assert(lp8 == namespace_of_8680s_Lua_Pack)
-		wesnoth.require(dir_of_8680s_Lua_Pack .. script .. ".lua")
+		local alreadyLoaded = lp8[lib]
+		if not alreadyLoaded then
+			lp8[lib] = wesnoth.require(lp8.lp8dir .. lib .. ".lua")
+		end
+		return lp8[lib], alreadyLoaded
 	end
+	lp8.require 'load'
 end
 
 return lp8.require
