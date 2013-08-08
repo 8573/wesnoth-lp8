ec93eee383c176522e143c3898c9cc46ce3cc931
8573 <8573dd@gmail.com>
2013-08-08 23:10:10 +0000

    Convert `wml` to use `load` lib.

--- 8680s_Lua_Pack/wml.lua
+++ 8680s_Lua_Pack/wml.lua
@@ -3,6 +3,8 @@
 
 lp8.require "utils"
 
+lp8.newLib 'wml'
+
 local type, getmetatable, pcall, pairs, ipairs =
 	type, getmetatable, pcall, pairs, ipairs
 local flip, tblorudt = lp8.flip, lp8.tblorudt
@@ -11,7 +13,7 @@ local at, et, match = {lp8.AND}, {}
 local function isCfg(x)
 	return getmetatable(x) == 'wml object'
 end
-lp8.is_cfg = isCfg
+lp8.export(isCfg, 'is_cfg')
 
 local function isTag(x)
 	if not tblorudt(x) then
@@ -22,20 +24,20 @@ local function isTag(x)
 	end)
 	return s and c and tblorudt(c)
 end
-lp8.is_tag = isTag
+lp8.export(isTag, 'is_tag')
 
 local function toCfg(x, f)
 	return isTag(x) and (match(x, f) and x[2] or error(
 		("expected tag input to match filter %q"): format(tostring(f)))) or x
 end
-lp8.to_cfg = toCfg
+lp8.export(toCfg, 'to_cfg')
 
 local function toTag(x, name)
 	return isTag(x) and x or {name or "dummy", x}
 end
-lp8.to_tag = toTag
+lp8.export(toTag, 'to_tag')
 
-function lp8.is_subtag(p, c)
+local function isSubtag(p, c)
 	p = toCfg(p)
 	for i = 1, #p do
 		if p[i] == c then
@@ -43,8 +45,9 @@ function lp8.is_subtag(p, c)
 		end
 	end
 end
+lp8.export(isSubtag, 'is_subtag')
 
-function lp8.is_child(p, c)
+local function isChild(p, c)
 	p = toCfg(p)
 	for i = 1, #p do
 		if p[i][2] == c then
@@ -52,14 +55,17 @@ function lp8.is_child(p, c)
 		end
 	end
 end
+lp8.export(isChild, 'is_child')
 
-function lp8.tags_equal(x, y)
+local function tagsEqual(x, y)
 	return x[1] == y[1] and match(x, y) and match(y, x)
 end
+lp8.export(tagsEqual, 'tags_equal')
 
-function lp8.cfgs_equal(x, y)
-	return lp8.tags_equal(toTag(x), toTag(y))
+local function cfgsEqual(x, y)
+	return tagsEqual(toTag(x), toTag(y))
 end
+lp8.export(cfgsEqual, 'cfgs_equal')
 
 local function getSubtag(p, f, n, i)
 	p = toCfg(p)
@@ -73,44 +79,49 @@ local function getSubtag(p, f, n, i)
 		end
 	end
 end
-lp8.get_subtag = getSubtag
+lp8.export(getSubtag, 'get_subtag')
 
-function lp8.subtags(p, f, i)
+local function subtags(p, f, i)
 	return function(s)
 		local t, i = getSubtag(p, f, 1, s.i + 1)
 		s.i = i
 		return t, i
 	end, {i = i or 0}
 end
+lp8.export(subtags, 'subtags')
 
-function lp8.children(p, f, i)
+local function children(p, f, i)
 	return function(s)
 		local c, i = getSubtag(p, f, 1, s.i + 1)
 		s.i = i
 		return (c or et)[2], i
 	end, {i = i or 0}
 end
+lp8.export(children, 'children')
 
-function lp8.get_subtags(p, f, b)
+local function getSubtags(p, f, b)
 	local r = {}
 	for t in lp8.subtags(p, f) do
 		r[#r+1] = t
 	end
 	return b and flip(r) or r
 end
+lp8.export(getSubtag, 'get_subtag')
 
-function lp8.get_child(p, f, n, i)
+local function getChild(p, f, n, i)
 	p, i = getSubtag(p, f, n, i)
 	return (p or et)[2], i
 end
+lp8.export(getChild, 'get_child')
 
-function lp8.get_children(p, f, b)
+local function getChildren(p, f, b)
 	local r = {}
 	for c in lp8.children(p, f) do
 		r[#r+1] = c
 	end
 	return b and flip(r) or r
 end
+lp8.export(getChildren, 'get_children')
 
 local tr = table.remove
 
@@ -128,14 +139,15 @@ local function removeSubtag(p, f, n, i)
 		end
 	end
 end
-lp8.remove_subtag = removeSubtag
+lp8.export(removeSubtag, 'remove_subtag')
 
-function lp8.remove_child(p, f, n, i)
+local function removeChild(p, f, n, i)
 	p, i = removeSubtag(p, f, n, i)
 	return (p or et)[2], i
 end
+lp8.export(removeChild, 'remove_child')
 
-function lp8.remove_subtags(p, f, b)
+local function removeSubtags(p, f, b)
 	p = toCfg(p)
 	local r = {}
 	for i = #p, 1, -1 do
@@ -147,8 +159,9 @@ function lp8.remove_subtags(p, f, b)
 	end
 	return b and r or flip(r)
 end
+lp8.export(removeSubtags, 'remove_subtags')
 
-function lp8.remove_children(p, f, b)
+local function removeChildren(p, f, b)
 	p = toCfg(p)
 	local r = {}
 	for i = #p, 1, -1 do
@@ -160,8 +173,9 @@ function lp8.remove_children(p, f, b)
 	end
 	return b and r or flip(r)
 end
+lp8.export(removeChildren, 'remove_children')
 
-function lp8.erase_subtags(p, f)
+local function eraseSubtags(p, f)
 	p = toCfg(p)
 	for i = #p, 1, -1 do
 		if match(p[i], f) then
@@ -170,10 +184,12 @@ function lp8.erase_subtags(p, f)
 		end
 	end
 end
+lp8.export(eraseSubtags, 'erase_subtags')
 
-function lp8.is_unit_proxy(x)
+local function isUnitProxy(x)
 	return getmetatable(x) == 'unit'
 end
+lp8.export(isUnitProxy, 'is_unit_proxy')
 
 function match(t, f)
 	local ty = type(f)
@@ -218,12 +234,14 @@ function match(t, f)
 	end
 	return f == nil or error(ty .. "s as filters not supported", 2)
 end
-lp8.match_tag = match
+lp8.export(match, 'match_tag')
 
-function lp8.to_unit_cfg(u)
-	local p = lp8.is_unit_proxy(u); return p and u.__cfg
+local function toUnitCfg(u)
+	local p = isUnitProxy(u); return p and u.__cfg
 		or tblorudt(u) and u or error(
 			("expected unit proxy or unit cfg; received %s with metatable %q"):
 				format(type(u), tostring(getmetatable(u)))), p
 end
+lp8.export(toUnitCfg, 'to_unit_cfg')
 
+return lp8.export()
