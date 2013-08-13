7d8e17794144720d82e3df59fa9e207c39b0d86a
8573 <8573dd@gmail.com>
2013-08-13 17:13:29 +0000

    Use `type` library in `utils`.

--- 8680s_Lua_Pack/utils.lua
+++ 8680s_Lua_Pack/utils.lua
@@ -2,13 +2,15 @@
 -- By 8680.
 
 lp8.require 'butils'
+lp8.require 'type'
 
 lp8.reopenLib 'utils'
 
 local type, tostring = type, tostring
+local typeof = lp8.Type.of
 
 local function dbgstr(x)
-	local ty = type(x)
+	local ty = typeof(x) or type(x)
 	if ty == 'table' or ty == 'function' or ty == 'userdata'
 			or x == nil then
 		return tostring(x)
